89e7a01a38facb7ce817a050731922ee95190d06
Dipan-17 <93267025+Dipan-17@users.noreply.github.com>
2024-06-05 10:30:49 +0530

    Intro screen added

--- app/src/main/java/dipan/ProjectManagement/IntroActivity.kt
+++ app/src/main/java/dipan/ProjectManagement/IntroActivity.kt
@@ -2,10 +2,17 @@ package dipan.ProjectManagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        //set the app to full screen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
 }
