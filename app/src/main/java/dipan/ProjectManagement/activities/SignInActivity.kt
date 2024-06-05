95beeddfc2b08229567ab508eb51d240f3c4559a
Dipan-17 <93267025+Dipan-17@users.noreply.github.com>
2024-06-05 13:08:58 +0530

    Sign in added

--- app/src/main/java/dipan/ProjectManagement/activities/SignInActivity.kt
+++ app/src/main/java/dipan/ProjectManagement/activities/SignInActivity.kt
@@ -1,15 +1,22 @@
 package dipan.ProjectManagement.activities
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import dipan.ProjectManagement.R
 import dipan.ProjectManagement.databinding.ActivitySignInBinding
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
 
         private var binding: ActivitySignInBinding? = null
 
+        private lateinit var auth:FirebaseAuth
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             binding= ActivitySignInBinding.inflate(layoutInflater)
@@ -23,8 +30,60 @@ class SignInActivity : AppCompatActivity() {
 
             setupActionBar()
 
+            auth=FirebaseAuth.getInstance()
+
+
+            binding?.btnSignIn?.setOnClickListener {
+                signInRegisteredUser()
+            }
         }
-        private fun setupActionBar(){
+
+        private fun signInRegisteredUser(){
+            val email=binding?.etEmail?.text.toString().trim{it <=' '}
+            val password=binding?.etPassword?.text.toString().trim{it <=' '}
+
+            if(validateForm(email, password)){
+                showProgressDialog(resources.getString(R.string.please_wait))
+
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        hideProgressDialog()
+
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("SIGN IN", "signInWithEmail:success")
+                            val user = auth.currentUser
+
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("SIGN IN", "signInWithEmail:failure", task.exception)
+                            showErrorSnackBar(task!!.exception!!.message.toString())
+
+                        }
+                    }
+            }
+        }
+
+    private fun validateForm(email:String,password:String):Boolean{
+        return when{
+            TextUtils.isEmpty(email)->{
+                showErrorSnackBar("Please enter an email")
+                false
+            }
+            TextUtils.isEmpty(password)->{
+                showErrorSnackBar("Please enter a password")
+                false
+            }
+            else->{
+                true
+            }
+        }
+    }
+
+    private fun setupActionBar(){
             setSupportActionBar(binding?.toolbarSignInActivity)
             val actionBar = supportActionBar
             if(actionBar != null){
